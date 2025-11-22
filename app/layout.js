9de86e5b7dd0fcf6b9128a21ae6e5e352ee68c94
lunianfu1986 <lunianfu1986@gmail.com>
2025-11-22 22:24:08 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Bad Time Simulator - Are You Ready for a Bad Time?',
+  description: 'Bad Time Simulator - Fight Sans from Undertale. Test your skills in the hardest boss battle!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
