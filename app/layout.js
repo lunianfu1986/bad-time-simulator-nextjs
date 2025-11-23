f52141d81fd57cce8fe3e09723b7982d6d6c4f3f
lunianfu1986 <lunianfu1986@gmail.com>
2025-11-23 09:32:23 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,34 @@
+// app/layout.js
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Bad Time Simulator - Are You Ready for a Bad Time?',
-  description: 'Bad Time Simulator - Fight Sans from Undertale. Test your skills in the hardest boss battle!',
+  description:
+    'Bad Time Simulator - Fight Sans from Undertale. Test your skills in the hardest boss battle!',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head />
+      <body>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6Q503TXNXR"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6Q503TXNXR');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   )
 }
-
