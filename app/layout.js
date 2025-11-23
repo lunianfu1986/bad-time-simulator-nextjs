3be51c2163e2563e8203e1970395eba498a5b95c
lunianfu1986 <lunianfu1986@gmail.com>
2025-11-23 09:26:23 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,3 +12,12 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6Q503TXNXR"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6Q503TXNXR');
+</script>
